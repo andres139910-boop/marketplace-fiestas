44ef9bf8e694697c0c7b3f3f2352319d3a5ad49c
Andres Doria <andres.139910@gmail.com>
2026-02-25 16:48:56 -0500

    Renombrar FiestaShop a PartyShop Cartagena

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.config['MAIL_PORT']           = 587
 app.config['MAIL_USE_TLS']        = True
 app.config['MAIL_USERNAME']       = 'andres.139910@gmail.com'
 app.config['MAIL_PASSWORD']       = 'oeqzynebреuzwxlo'
-app.config['MAIL_DEFAULT_SENDER'] = 'FiestaShop <andres.139910@gmail.com>'
+app.config['MAIL_DEFAULT_SENDER'] = 'PartyShop Cartagena <andres.139910@gmail.com>'
 
 EXTENSIONES_PERMITIDAS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 
